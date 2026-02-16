d3fb2d038f8dd3d7ea05fbeb997ffdfaa0e31dc8
Ádám Horváth <vorhdam@gmail.com>
2026-02-16 15:07:39 +0100

    Updated registry to fix file locations

--- src/registry/new-york/flags/ui/flag.tsx
+++ src/registry/new-york/flags/ui/flag.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { type FlagCode } from "../lib/flags";
-import { useFlag } from "../hooks/useFlag";
+import { type FlagCode } from "@/lib/flags";
+import { useFlag } from "@/hooks/useFlag";
 
 export type FlagProps = React.SVGProps<SVGSVGElement> & {
   code: FlagCode;
